599476622445468c79bf73d3155a193be8a9e7c3
Stefan Helber <stefan.helber@prod.uni-hannover.de>
2015-01-05 17:18:38 +0100

    Done with 9.2.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,17 @@ class ApplicationController < ActionController::Base
     super
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
 end
